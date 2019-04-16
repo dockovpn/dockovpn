a0f33c901b0f363194f9bb4d340207d21474a758
Alexander Litvinenko <alekslitvinenk@gmail.com>
2019-04-16 14:58:04 +0300

    Fix server-setup.sh to work in docker ubuntu

--- scripts/server-setup.sh
+++ scripts/server-setup.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-sudo apt-get install openvpn easy-rsa
+apt-get update
+echo 'Y' | apt-get install openvpn easy-rsa
 make-cadir ~/openvpn-ca
 cd ~/openvpn-ca
 
 #need to edit vars somehow or copy entire file from somewhere
 #nano vars
-
 source vars
 ./clean-all
-./build-ca
+echo 'n\n\n\n\n\n\n\n\' | ./build-ca
 
-./build-key-server server
+echo 'n\n\n\n\n\n\n\n\n\n\yn\yn\' | ./build-key-server server
 #need not to create challenging password
 #need to answer y(yes) when asked to sign and commit certificate
 
 #Build strong Deffi-Haufman key
 ./build-dh
-
