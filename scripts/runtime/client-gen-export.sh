a94241e0932a314ab0234c6f380276ad3c9a10d6
Alexander Litvinenko <alekslitvinenk@gmail.com>
2019-05-28 21:37:01 +0300

    Fix download file name

--- scripts/runtime/client-gen-export.sh
+++ scripts/runtime/client-gen-export.sh
@@ -72,4 +72,4 @@ case $FLAGS in
         ;;
 esac
 
-{ echo -ne "HTTP/1.1 200 OK\r\nContent-Length: $(wc -c <$FILE_PATH)\r\nContent-Type: $CONTENT_TYPE\r\n\Content-Disposition: attachment; filename=$FILE_NAMEr\n\Accept-Ranges: bytes\r\n\r\n"; cat $FILE_PATH; } | nc -w0 -l 8080
+{ echo -ne "HTTP/1.1 200 OK\r\nContent-Length: $(wc -c <$FILE_PATH)\r\nContent-Type: $CONTENT_TYPE\r\nContent-Disposition: attachment; fileName=\"$FILE_NAME\"\r\nAccept-Ranges: bytes\r\n\r\n"; cat $FILE_PATH; } | nc -w0 -l 8080
