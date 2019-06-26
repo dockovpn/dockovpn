e21219f2846698a4e0ebe06c7f483b8c77f84a81
Alexander Litvinenko <alekslitvinenk@gmail.com>
2019-06-26 23:13:07 +0300

    Make build.sh cicd pipeline ready

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 rm -r client
 
 # bash generate random 10 character alphanumeric string (upper and lowercase) and 
 # NEW_UUID=$(env LC_CTYPE=C tr -dc "a-zA-Z0-9" < /dev/urandom | head -c 10 | cat)
-NEW_UUID=snapshot
+NEW_UUID=nightly
 
-docker build -t alekslitvinenk/openvpn:$NEW_UUID -t alekslitvinenk/openvpn:latest --no-cache .
+docker build -t alekslitvinenk/openvpn:$NEW_UUID --no-cache .
+docker push alekslitvinenk/openvpn:$NEW_UUID
