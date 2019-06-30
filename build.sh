11f57ac4cf45fc3bf3d08bfadb16a1f701444267
Alexander Litvinenko <alekslitvinenk@gmail.com>
2019-06-30 14:36:41 +0300

    Nightly builds with latest tag

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ rm -r client
 
 # bash generate random 10 character alphanumeric string (upper and lowercase) and 
 # NEW_UUID=$(env LC_CTYPE=C tr -dc "a-zA-Z0-9" < /dev/urandom | head -c 10 | cat)
-NEW_UUID=nightly
+NEW_UUID=latest
 
 docker build -t alekslitvinenk/openvpn:$NEW_UUID --no-cache .
 docker push alekslitvinenk/openvpn:$NEW_UUID
