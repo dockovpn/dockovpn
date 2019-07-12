db78dfba8f67c74873424d95ebd385f202b82943
Alexander Litvinenko <array.shift@yahoo.com>
2019-07-12 17:47:57 +0300

    Fix typo

--- scripts/runtime/setup-tuntap-device.sh
+++ scripts/runtime/setup-tuntap-device.sh
@@ -5,6 +5,6 @@ source $APP_INSTALL_PATH/runtime/functions.sh
 mkdir -p /dev/net
 
 if [ ! -c /dev/net/tun ]; then
-    echo "$(datef) Creating tn/tap device"
+    echo "$(datef) Creating tun/tap device."
     mknod /dev/net/tun c 10 200
 fi
