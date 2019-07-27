4be60fe0adc5704b5bde9ea74b89722f3bc7db38
Alexander Litvinenko <alekslitvinenk@gmail.com>
2019-07-27 14:02:40 +0300

    Improve build script

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-rm -r client
-
-# bash generate random 10 character alphanumeric string (upper and lowercase) and 
-# NEW_UUID=$(env LC_CTYPE=C tr -dc "a-zA-Z0-9" < /dev/urandom | head -c 10 | cat)
 NEW_UUID=latest
 
-docker build -t alekslitvinenk/openvpn:$NEW_UUID --no-cache .
+docker build -t alekslitvinenk/openvpn:$NEW_UUID .
 docker push alekslitvinenk/openvpn:$NEW_UUID
