f18e70dd7c598d575fbead000089c615520a39cc
Alexander Litvinenko <alekslitvinenk@gmail.com>
2019-12-10 21:36:00 +0200

    Update build.sh to tag docker images with right version

--- build.sh
+++ build.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-NEW_UUID=latest
+MAJOR_VERSION=0 # Updated manually
+MINOR_VERSION=36 # Updated manually
+BUILD_NUMBER=0
 
-docker build -t alekslitvinenk/openvpn:$NEW_UUID . --no-cache
-docker push alekslitvinenk/openvpn:$NEW_UUID
+# Create file build_number.txt if it doesn't exist
+#FILE=build_number.txt
+#if test ! -f "$FILE"
+#then
+#    echo 0 > $FILE
+#fi
+
+# Increment build number
+#BUILD_NUMBER=$(($(cat $FILE)+1)) # Updated automatically
+#echo $BUILD_NUMBER > $FILE
+
+FULL_VESRION="v$MAJOR_VERSION.$MINOR_VERSION.$BUILD_NUMBER"
+
+docker build -t alekslitvinenk/openvpn:$FULL_VESRION -t alekslitvinenk/openvpn:latest . --no-cache
+docker push alekslitvinenk/openvpn:$FULL_VESRION
+docker push alekslitvinenk/openvpn:latest
+
+echo $FULL_VESRION
