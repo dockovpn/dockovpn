3223bb440b958cbc61b8d028d787faa039f155fd
Alexander Litvinenko <alekslitvinenk@gmail.com>
2019-12-11 22:06:48 +0200

    Add RELEASE_GUIDELINE.md

--- build.sh
+++ build.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-MAJOR_VERSION=0 # Updated manually
-MINOR_VERSION=36 # Updated manually
-BUILD_NUMBER=0
-
-# Create file build_number.txt if it doesn't exist
-#FILE=build_number.txt
-#if test ! -f "$FILE"
-#then
-#    echo 0 > $FILE
-#fi
-
-# Increment build number
-#BUILD_NUMBER=$(($(cat $FILE)+1)) # Updated automatically
-#echo $BUILD_NUMBER > $FILE
-
-FULL_VESRION="v$MAJOR_VERSION.$MINOR_VERSION.$BUILD_NUMBER"
+FULL_VESRION="$(cat < 'full_version.txt')-regen-dh"
 
 docker build -t alekslitvinenk/openvpn:$FULL_VESRION -t alekslitvinenk/openvpn:latest . --no-cache
 docker push alekslitvinenk/openvpn:$FULL_VESRION
