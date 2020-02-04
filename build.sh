3df7122ad73e10b437db7a817b8b3275d29360c1
Alexander Litvinenko <alekslitvinenk@gmail.com>
2020-02-04 21:32:29 +0200

    Update build scripts after VERSION file was introduced

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FULL_VESRION="$(cat < 'full_version.txt')-regen-dh"
+FULL_VESRION="$(cat ./VERSION)-regen-dh"
 
 docker build -t alekslitvinenk/openvpn:$FULL_VESRION -t alekslitvinenk/openvpn:latest . --no-cache
 docker push alekslitvinenk/openvpn:$FULL_VESRION
