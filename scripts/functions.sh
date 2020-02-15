aec2ae7ed784bd2155ccb04095ce4fb8c68b8e03
Alexander Litvinenko <alekslitvinenk@gmail.com>
2020-02-16 00:16:06 +0200

    Assign clint folder a unique path

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -13,10 +13,12 @@ function createConfig() {
     # Client sertificate /usr/share/easy-rsa/pki/issued/client.crt
     # CA is by the path /usr/share/easy-rsa/pki/ca.crt
 
-    # Create mkdir if doesn't exist
-    mkdir -p client
+    CLIENT_ID="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
+    CLIENT_PATH="clients/$CLIENT_ID"
 
-    cp pki/private/client.key pki/issued/client.crt pki/ca.crt /etc/openvpn/ta.key client
+    mkdir -p $CLIENT_PATH
+
+    cp pki/private/client.key pki/issued/client.crt pki/ca.crt /etc/openvpn/ta.key $CLIENT_PATH
 
     # Set default value to HOST_ADDR if it was not set from environment
     if [ -z "$HOST_ADDR" ]
@@ -25,33 +27,36 @@ function createConfig() {
     fi
 
     cd $APP_INSTALL_PATH
-    cp config/client.ovpn client
+    cp config/client.ovpn $CLIENT_PATH
 
-    echo -e "\nremote $HOST_ADDR 1194" >> client/client.ovpn
+    echo -e "\nremote $HOST_ADDR 1194" >> "$CLIENT_PATH/client.ovpn"
 
     # Embed client authentication files into config file
     cat <(echo -e '<ca>') \
-        client/ca.crt <(echo -e '</ca>\n<cert>') \
-        client/client.crt <(echo -e '</cert>\n<key>') \
-        client/client.key <(echo -e '</key>\n<tls-auth>') \
-        client/ta.key <(echo -e '</tls-auth>') \
-        >> client/client.ovpn
+        "$CLIENT_PATH/ca.crt" <(echo -e '</ca>\n<cert>') \
+        "$CLIENT_PATH/client.crt" <(echo -e '</cert>\n<key>') \
+        "$CLIENT_PATH/client.key" <(echo -e '</key>\n<tls-auth>') \
+        "$CLIENT_PATH/ta.key" <(echo -e '</tls-auth>') \
+        >> "$CLIENT_PATH/client.ovpn"
 
-    echo "$(datef) Client.ovpn file has been generated"
+    echo $CLIENT_PATH
 }
 
 function zipFiles() {
+    CLIENT_PATH="$1"
     # -q to silence zip output
-    zip -q client.zip client/client.ovpn
-    cp client.zip client
+    zip -q client.zip "$CLIENT_PATH/client.ovpn"
+    cp client.zip "$CLIENT_PATH"
 
-    echo "$(datef) Client.zip file has been generated"
+    echo "$(datef) $CLIENT_PATH/client.zip file has been generated"
 }
 
 function zipFilesWithPassword() {
+    CLIENT_PATH="$1"
+    ZIP_PASSWORD="$2"
     # -q to silence zip output
-    zip -q -P "$1" client.zip client/client.ovpn
-    cp client.zip client
+    zip -q -P "$ZIP_PASSWORD" client.zip "$CLIENT_PATH/client.ovpn"
+    cp client.zip "$CLIENT_PATH"
 
-    echo "$(datef) Client.zip with password protection has been generated"
+    echo "$(datef) $CLIENT_PATH/client.zip with password protection has been generated"
 }
--- scripts/start.sh
+++ scripts/start.sh
@@ -73,15 +73,16 @@ FLAGS=$1
 # Switch statement
 case $FLAGS in
     c)
-        createConfig
+        CLIENT_PATH="$(createConfig)"
 
         CONTENT_TYPE=application/text
         FILE_NAME=client.ovpn
-        FILE_PATH=client/$FILE_NAME
+        FILE_PATH="$CLIENT_PATH/$FILE_NAME"
+        echo "$(datef) $FILE_PATH/client.ovpn file has been generated"
         ;;
     cz)
-        createConfig
-        zipFiles
+        CLIENT_PATH="$(createConfig)"
+        zipFiles "$CLIENT_PATH"
 
         CONTENT_TYPE=application/zip
         FILE_NAME=client.zip
@@ -93,8 +94,8 @@ case $FLAGS in
         then
             echo "Not enogh arguments" && exit 0
         else
-            createConfig
-            zipFilesWithPassword "$2"
+            CLIENT_PATH="$(createConfig)"
+            zipFilesWithPassword "$CLIENT_PATH" "$2"
 
             CONTENT_TYPE=application/zip
             FILE_NAME=client.zip
@@ -106,7 +107,7 @@ esac
 echo "$(datef) Config server started, download your $FILE_NAME config at http://$HOST_ADDR/"
 echo "$(datef) NOTE: After you download you client config, http server will be shut down!"
 
-{ echo -ne "HTTP/1.1 200 OK\r\nContent-Length: $(wc -c <$FILE_PATH)\r\nContent-Type: $CONTENT_TYPE\r\nContent-Disposition: attachment; fileName=\"$FILE_NAME\"\r\nAccept-Ranges: bytes\r\n\r\n"; cat $FILE_PATH; } | nc -w0 -l 8080
+{ echo -ne "HTTP/1.1 200 OK\r\nContent-Length: $(wc -c <$FILE_PATH)\r\nContent-Type: $CONTENT_TYPE\r\nContent-Disposition: attachment; fileName=\"$FILE_NAME\"\r\nAccept-Ranges: bytes\r\n\r\n"; cat "$FILE_PATH"; } | nc -w0 -l 8080
 
 echo "$(datef) Config http server has been shut down"
 
