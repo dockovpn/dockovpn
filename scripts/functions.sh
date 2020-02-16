d1df1c1f0cd0d6902bed583f20db6eabb21c7158
Alexander Litvinenko <alekslitvinenk@gmail.com>
2020-02-16 21:53:20 +0200

    Generate unique client files

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -7,18 +7,18 @@ function datef() {
 }
 
 function createConfig() {
+    CLIENT_ID="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
+    CLIENT_PATH="clients/$CLIENT_ID"
+
     # Redirect stderr to the black hole
-    /usr/share/easy-rsa/easyrsa build-client-full client nopass &> /dev/null
+    /usr/share/easy-rsa/easyrsa build-client-full "$CLIENT_ID" nopass &> /dev/null
     # Writing new private key to '/usr/share/easy-rsa/pki/private/client.key
     # Client sertificate /usr/share/easy-rsa/pki/issued/client.crt
     # CA is by the path /usr/share/easy-rsa/pki/ca.crt
 
-    CLIENT_ID="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
-    CLIENT_PATH="clients/$CLIENT_ID"
-
     mkdir -p $CLIENT_PATH
 
-    cp pki/private/client.key pki/issued/client.crt pki/ca.crt /etc/openvpn/ta.key $CLIENT_PATH
+    cp "pki/private/$CLIENT_ID.key" "pki/issued/$CLIENT_ID.crt" pki/ca.crt /etc/openvpn/ta.key $CLIENT_PATH
 
     # Set default value to HOST_ADDR if it was not set from environment
     if [ -z "$HOST_ADDR" ]
@@ -34,8 +34,8 @@ function createConfig() {
     # Embed client authentication files into config file
     cat <(echo -e '<ca>') \
         "$CLIENT_PATH/ca.crt" <(echo -e '</ca>\n<cert>') \
-        "$CLIENT_PATH/client.crt" <(echo -e '</cert>\n<key>') \
-        "$CLIENT_PATH/client.key" <(echo -e '</key>\n<tls-auth>') \
+        "$CLIENT_PATH/$CLIENT_ID.crt" <(echo -e '</cert>\n<key>') \
+        "$CLIENT_PATH/$CLIENT_ID.key" <(echo -e '</key>\n<tls-auth>') \
         "$CLIENT_PATH/ta.key" <(echo -e '</tls-auth>') \
         >> "$CLIENT_PATH/client.ovpn"
 
@@ -47,7 +47,6 @@ function zipFiles() {
     # -q to silence zip output
     # -j junk directories
     zip -q -j "$CLIENT_PATH/client.zip" "$CLIENT_PATH/client.ovpn"
-    #cp client.zip "$CLIENT_PATH"
 
     echo "$(datef) $CLIENT_PATH/client.zip file has been generated"
 }
@@ -59,7 +58,6 @@ function zipFilesWithPassword() {
     # -j junk directories
     # -P pswd use standard encryption, password is pswd
     zip -q -j -P "$ZIP_PASSWORD" "$CLIENT_PATH/client.zip" "$CLIENT_PATH/client.ovpn"
-    #cp client.zip "$CLIENT_PATH"
 
     echo "$(datef) $CLIENT_PATH/client.zip with password protection has been generated"
 }
