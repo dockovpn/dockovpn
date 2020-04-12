6329858b1e1787c452c21e6e0c3172ed75d80f42
Alexander Litvinenko <alekslitvinenk@gmail.com>
2020-04-12 14:54:12 +0300

    Issues/86 (#90)
    * Generate LOCKFILE if wasn't created
    
    * Persist certs and PKI
    
    * Update docs

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 docker run --cap-add=NET_ADMIN \
+-v openvpn_conf:/opt/Dockovpn \
 -p 1194:1194/udp -p 80:8080/tcp \
 -e HOST_ADDR=localhost \
---name dockovpn --rm \
+--rm \
 alekslitvinenk/openvpn "$@"
